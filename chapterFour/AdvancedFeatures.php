fa9c34f53669fc8dd5046032bde5156f0057e573
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2018-03-21 11:08:56 +0100

    create folder/file for chapter 4

---
+++ chapterFour/AdvancedFeatures.php
@@ -0,0 +1,16 @@
+<?php
+/************************************************************* Advanced Feautres ************************************************************/
+
+/*
+*	Chapter covers:
+*	Static methods and properties: Accessing data and functionality through classes rather than objects
+*	Abstract classes and interfaces: Separating design from implementation
+*	Traits: Sharing implementation between class hierarchies
+*	Error handling: Introducing exceptions
+*	Final classes and methods: Limiting inheritance
+*	Interceptor methods: Automating delegation
+*	Destructor methods: Cleaning up after your objects
+*	Cloning objects: Making object copies
+*	Resolving objects to strings: Creating a summary method
+*	Callbacks: Adding functionality to components with anonymous functions
+*/
