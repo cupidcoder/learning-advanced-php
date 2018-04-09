4587e500e1da0ee701cd34efd72be7c99277e809
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2018-04-09 16:20:44 +0100

    finish chapter 7

--- chapterSix/ObjectsAndDesign.php
+++ chapterSix/ObjectsAndDesign.php
@@ -7,8 +7,20 @@
 *	Class Scope: Knowing what to include in a class
 *	Encapsulation: Hiding class details and implementation behind's a class interface
 *	Polymorphism: Using a common supertype to allow the transparent substitution of specialized subtypes at runtime
-*	UML: Using diagrams to describe object-oriented architectures
+*	UML (Unified Modelling Language): Using diagrams to describe object-oriented architectures
 */
 
 /********************   Design Basics **********************/
 
+// Use of OOP does not imply Object oriented design
+
+// Object-oriented programming through solid Object oriented design is preferrable to Proceedural programming because objects allow for
+// individual components in a system to be independent of other components leading to a less-cohesive, decoupled and orthogonal system 
+
+
+// -- Program to an interface, not to an implementation --
+
+
+/*********** UML (Unified Modelling Language): Using diagrams to describe object-oriented architectures ***********/
+
+// Graphical representation of an overview of an OOP design, indication the relationship and structure of classes (including their properties and methods)
