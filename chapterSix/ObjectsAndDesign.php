8a62dcd1457738cfc506dfe7c13f5005f0e475a8
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2018-04-09 13:31:30 +0100

    create folder/file for chapter 6

---
+++ chapterSix/ObjectsAndDesign.php
@@ -0,0 +1,14 @@
+<?php
+/************************************************************* Objects and Design ************************************************************/
+
+/*
+*	Chapter covers:
+*	Design Basics
+*	Class Scope: Knowing what to include in a class
+*	Encapsulation: Hiding class details and implementation behind's a class interface
+*	Polymorphism: Using a common supertype to allow the transparent substitution of specialized subtypes at runtime
+*	UML: Using diagrams to describe object-oriented architectures
+*/
+
+/********************   Design Basics **********************/
+
