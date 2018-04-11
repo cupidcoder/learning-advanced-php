ed2290a588dce7640c50ee0e69d87fed69cf08ed
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2018-04-11 14:51:36 +0100

    create folder/file for chapter 9

---
+++ chapterNine/GeneratingObjects.php
@@ -0,0 +1,11 @@
+<?php
+/************************************************************* Pattern for generating objects ************************************************************/
+
+/*
+*	Chapter covers:
+*	The Singleton pattern
+*	The Factory pattern
+*	The Abstract Factory pattern
+*	The Prototype pattern
+*/
+
