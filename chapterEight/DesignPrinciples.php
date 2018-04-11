d4fefe48b05b4f78cac2ee907d9bc4360c5aafba
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2018-04-11 14:46:52 +0100

    finish chapter 8

---
+++ chapterEight/DesignPrinciples.php
@@ -0,0 +1,28 @@
+<?php
+/************************************************************* Some Design Princples ************************************************************/
+
+/*
+*	Chapter covers:
+*	Composition: Using Composition and Aggregation to promote greater Flexibility
+*	Decoupling: Reducing component dependency across elements in the system
+*	Interface: Utilising interface in ensuring Encapsulation
+*	Categories of Patterns: Categories of patterns to be discussed in this book
+*/
+
+// Design patterns are primarily suited to solve problems that are unique and best suited to the design pattern, but in solving these problems, design patterns generally ensure that 
+// the codebase promotes Reusability and Flexibility
+
+
+/********* Composition: Using Composition and Aggregation to promote greater Flexibility **************/
+//	Employing composition ensures a level of flexibility that inheritance alone cannot allow especially when classes take on multiple responsibilities
+
+// Under composition, delegation means invoking another object's method inside a current instance to perform a specific task
+
+
+/*********** Categories of Patterns: Categories of patterns to be discussed in this book ***********/
+
+// 1. Generating objects
+// 2. Organizing objects and classes
+// 3. Task-oriented patterns
+// 4. Enterprise patterns
+// 5. Database patterns
