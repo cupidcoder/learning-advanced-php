1d44ba1169e28c211926016372dd9d05ce759ca6
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2018-04-30 11:44:10 +0100

    create folder/file for chapter 10

---
+++ chapterTen/ObjectOrientedPatterns.php
@@ -0,0 +1,9 @@
+<?php
+/************************************************************* Patterns for Flexible Object Programming ************************************************************/
+
+/*
+*	Chapter covers:
+*	The Composite pattern
+*	The Decorator pattern
+*	The Facade pattern
+*/
