d7d23f5484f6afcf1182e4aa1eabe3714e9e0136
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2018-05-10 16:16:12 +0100

    create folder/file for chapter 14

---
+++ chapterFourteen/GoodAndBadPractice.php
@@ -0,0 +1,12 @@
+<?php
+/************************************************************* Good (and Bad) Practice ************************************************************/
+
+/*
+*	Chapter covers:
+*	Third-party packages: Where to get them, when to use them
+*	Build: Creating and deploying packages
+*	Version control: Bringing harmony to the development process
+*	Documentation: Writing code that is easy to understand, use, and extend
+*	Unit testing: A tool for automated bug detection and prevention
+*	Continuous integration: Using this practice and set of tools to automate project builds and tests and be alerted of problems as they occur
+*/
