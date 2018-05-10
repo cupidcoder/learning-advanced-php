89663f406fff5d752c601496ecfa69a454f97efe
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2018-05-10 15:23:22 +0100

    create folder/file for chapter 12

---
+++ chapterTwelve/EnterprisePatterns.php
@@ -0,0 +1,13 @@
+<?php
+/************************************************************* Enterprise Patterns  ************************************************************/
+
+/*
+*	Chapter covers:
+*	Architecture overview: An introduction to the layers that typically comprise an enterprise application
+*	Registry pattern: Managing application data
+*	Presentation layer: Tools for managing and responding to requests and for presenting data to the user
+* 	Business logic layer: Getting to the real purpose of your system: addressing business problems
+*/
+
+/************  Architecture Overview *************/
+
