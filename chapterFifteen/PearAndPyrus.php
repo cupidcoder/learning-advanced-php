1c093c2eef2468c67bc1f02a12ebaa614077749b
Chukwudi Umeilechukwu <chukwudi.ume@gmail.com>
2018-05-11 11:15:25 +0100

    create folder/file for chapter 15

---
+++ chapterFifteen/PearAndPyrus.php
@@ -0,0 +1,12 @@
+<?php
+/************************************************************* PEAR and Pyrus ************************************************************/
+
+/*
+*	Chapter covers, by overviewing the things that will be  covered in subsequent chapters:
+*	PEAR basics: What is this strange fruit?
+*	Installing PEAR package installation: All it takes is one command.
+*	Working with Pyrus, PEAR’s younger sibling
+*	Adding PEAR packages to your projects: An example and some notes on error handling
+*	package.xml: The anatomy of a build file
+*	Creating your own channel: Providing transparent dependency management and package downloads for users.
+*/
